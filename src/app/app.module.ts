513733a932b88a8b9eb7faa6b2d4b2524f3010f9
hristo2612 <hristoapps@gmail.com>
2022-09-02 20:20:14 +0300

    Add Lazy Loaded 'Game' Module + Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/game/game.module').then(m => m.GameModule)
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
