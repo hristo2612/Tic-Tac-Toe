62fdb6d70a850e4bb892accaead43a7efbd5d364
hristo2612 <hristoapps@gmail.com>
2022-09-12 00:05:14 +0300

    Gif Url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  gifUrl: 'https://api.giphy.com/v1/gifs/random?api_key=5VWzikTONVTDeZD7saUk6gZfpXfzv6JL&tag=think&limit=6&rating=r'
 };
